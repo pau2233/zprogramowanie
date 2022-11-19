186d8e9e7bdbf58f32d9322715d35452f0b5f2cf
pau2233 <paula126@op.pl>
2022-11-19 22:32:47 +0000

    Push from SwaggerHub.

---
+++ node-js/service/DefaultService.js
@@ -0,0 +1,51 @@
+'use strict';
+
+
+/**
+ * Inverse Picture
+ *
+ * no response value expected for this operation
+ **/
+exports.inverse_picture_picture_invert_get = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Is Prime Number
+ *
+ * number Integer 
+ * no response value expected for this operation
+ **/
+exports.is_prime_number_prime__number__get = function(number) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Root
+ *
+ * no response value expected for this operation
+ **/
+exports.root__get = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * User
+ *
+ * no response value expected for this operation
+ **/
+exports.user_login_get = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
