1896b491eb75e0c82e7ba8bf2df95bbc3f83ad59
pau2233 <paula126@op.pl>
2022-11-25 23:12:49 +0100

    obrazek

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "By sprawdzić czy liczba jest pierwsza - http://localhost:8000/prime/10619863 lub https://zprogramowanie-grzanka.herokuapp.com/prime/10619863 " +
-            " By odwrócić kolory obrazka  - http://127.0.0.1:8000/picture/invert/image.jpg"
+    return {"message": "By sprawdzić czy liczba jest pierwsza - /prime/10619863 " +
+            " Logowanie - /login, by odwrócić kolory obrazka - invert/image.jpg lub http://127.0.0.1:8000/picture/invert/image.jpg "
             }
 
 @app.get("/prime/{number}")
@@ -18,16 +18,14 @@ async def is_prime_number(number: int):
     else:
         return {"message": f"Liczba {number} nie jest pierwsza"}
 
-image=cv2.imread('image.jpg')
+#image=cv2.imread('image.jpg')
 
-@app.get("/picture/invert")
-async def inverse_picture():
-
+@app.get("/picture/invert/{image}")
+async def inverse_picture(image):
+    image = cv2.imread(image)
     newimage = ~image
     cv2.imwrite("img_inverted.jpg", newimage)
     return {"message": "Obrazek jest teraz nawiedzony"}
-
-
 
 
 @app.get("/login")
